0612f180b7147097f3b0b8842f97ea6add8ae0f2
Tim-M-U <timofejminaev361@gmail.com>
2025-09-16 12:20:51 +0300

    add prod m

--- Tim.py
+++ Tim.py
@@ -1,7 +1,11 @@
 def main():
     pass
 
-
+def prod_two_nums():
+    num1 = int(input('Enter num1: '))
+    num2 = int(input('Enter num2: '))
+    res = f'Multiply num1 and num2 is: {num1 * num2}'
+    return res
 if __name__ == '__main__':
     main()
 
