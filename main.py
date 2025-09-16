55e1a7fa893c0b402ee1e345f7555b7125e295ba
Tim-M-U <timofejminaev361@gmail.com>
2025-09-16 11:26:22 +0300

    add sum_two_nums M

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 def print_hello_world():
     return print('Hello world')
+
+def sum_two_nums():
+    num1 = int(input('Enter num1: '))
+    num2 = int(input('Enter num2: '))
+    res = f'Summ num1 and num2 is: {num1 + num2}'
+    return res
